33f06655a87dede8b8db0cb631e21019701c4bc4
Nicolas Haquet <nicolas.haquet@w6d.io>
2023-12-14 12:15:12 +0100

    [CLEAN] trait bound

--- src/utils/error.rs
+++ src/utils/error.rs
@@ -14,13 +14,10 @@ pub struct ErrorData<'a> {
 }
 
 #[async_trait]
-pub trait SendError {
+pub trait SendError: std::error::Error + Sync {
     ///send error to the given kafka topic
     #[cfg(not(tarpaulin_include))]
-    async fn send_error(&self, config: Arc<RwLock<OPAConfig>>)
-    where
-        Self: std::error::Error + Sync,
-    {
+    async fn send_error(&self, config: Arc<RwLock<OPAConfig>>) {
         let read_config = config.read().await;
         let config = &read_config.kafka;
         let error = ErrorData {
